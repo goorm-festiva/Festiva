569f97d4ba02b1f1a3f00c08429f16d931117d34
cho-eun-young <choeunyoung224@gmail.com>
2024-11-22 15:47:27 +0900

    feat : 캘린더로 축제 필터링하기

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,33 +1,29 @@
-import React, { useEffect } from "react";
+// src/components/EventList.js
+import React from "react";
 import { useFestivalStore } from "../store/useFestivalStore";
 
 const EventList = () => {
-  const { festivalData, isLoading, fetchFestivalData } = useFestivalStore();
+  const { filteredEvents } = useFestivalStore(); // Zustand 스토어에서 필터링된 이벤트 가져오기
 
-  useEffect(() => {
-    fetchFestivalData("축제");
-  }, [fetchFestivalData]);
-
-  if (isLoading) {
-    return <div>로딩 중...</div>;
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>등록된 이벤트가 없습니다.</p>;
   }
 
   return (
     <div>
-      {festivalData &&
-        festivalData.map((event, index) => (
-          <div key={index}>
-            <h2>{event.TITLE}</h2>
-            <p>{event.DATE}</p>
-            {event.MAIN_IMG && (
-              <img
-                style={{ width: "300px" }}
-                src={event.MAIN_IMG}
-                alt={event.TITLE}
-              />
-            )}
-          </div>
-        ))}
+      {filteredEvents.map((event, index) => (
+        <div key={index}>
+          <h2>{event.TITLE}</h2>
+          <p>{event.DATE}</p>
+          {event.MAIN_IMG && (
+            <img
+              style={{ width: "300px" }}
+              src={event.MAIN_IMG}
+              alt={event.TITLE}
+            />
+          )}
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+// src/components/FilterComponent.js
+import React, { useState } from "react";
+import { DateRange } from "react-date-range"; // 날짜 범위 선택기
+import "react-date-range/dist/styles.css"; // 스타일 가져오기
+import "react-date-range/dist/theme/default.css"; // 테마 가져오기
 
-const FilterComponent = () => {
+const FilterComponent = ({ onDateChange }) => {
+  const [dateRange, setDateRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
+  const handleSelect = (ranges) => {
+    setDateRange([ranges.selection]);
+    onDateChange(ranges.selection); // 부모 컴포넌트에 선택된 날짜 범위를 전달
+  };
+
   return (
-    <>
-      <div>
-        TopInfoBox
-        <input type="search" />
-        <div>ListFilter</div>
-      </div>
-    </>
+    <div>
+      <input type="search" placeholder="Search" />
+
+      <DateRange
+        ranges={dateRange}
+        onChange={handleSelect}
+        editableDateInputs={true}
+        moveRangeOnFirstSelection={false}
+      />
+    </div>
   );
 };
 
