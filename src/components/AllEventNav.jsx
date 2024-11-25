e9f8838f8265a667ab3a692a9914a6459e6a45c6
cho-eun-young <choeunyoung224@gmail.com>
2024-11-25 17:19:16 +0900

    feat : AllEvent Nav 스타일 컴포넌트 추가

--- src/components/AllEventNav.jsx
+++ src/components/AllEventNav.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const AllEventNav = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link> &gt; {/* '>' 기호로 구분 */}
-      <Link to="/AllEvent">행사 일정</Link> {/* AllEvent 페이지 링크 */}
-    </nav>
+    <NavWrapper>
+      <StyledLink to="/">Home</StyledLink>
+      <Separator>&gt;</Separator>
+      <StyledLink to="/AllEvent">행사 일정</StyledLink>
+    </NavWrapper>
   );
 };
+
+const NavWrapper = styled.nav`
+  padding: 10px;
+  margin-bottom: 20px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #333;
+  margin-right: 5px;
+
+  &:hover {
+    color: #007bff;
+  }
+`;
+
+const Separator = styled.span`
+  margin: 0 5px;
+  color: #666;
+`;
 
 export default AllEventNav;
