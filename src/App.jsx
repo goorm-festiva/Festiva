c9a18d5c1b4ed5d5e7ea6d15399b7318a0f9a388
yungan9 <geyun105@naver.com>
2024-11-26 01:49:23 +0900

    feat: 예약페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from "./Styles/GlobalStyles";
 import Home from "./pages/Home";
 import AllEvent from "./pages/AllEvent";
 import LoginPage from "./pages/LoginPage";
+import ReservationPage from "./pages/ReservationPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/AllEvent" element={<AllEvent />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/reservation" element={<ReservationPage />} />
       </Routes>
     </>
   );
