e3500150abdaae93ce603c44cec72501bb5805f2
cho-eun-young <choeunyoung224@gmail.com>
2024-11-26 16:50:30 +0900

    feat : 초기에 모든 이벤트 보여주기

--- src/store/festivalStore.jsx
+++ src/store/festivalStore.jsx
@@ -4,10 +4,11 @@ import axios from "axios";
 // Festival 데이터를 불러오는 스토어
 export const useFestivalStore = create((set) => ({
   festivalData: null,
+  filteredEvents: null, // 필터링된 이벤트를 위한 새로운 상태
   isLoading: true,
   fetchFestivalData: async (category, start = 1, end = 300) => {
     try {
-      const apiKey = import.meta.env.VITE_APP_FESTIVAL_API_KEY;
+      const apiKey = import.meta.env.VITE_APP_apiKey;
       const url = `http://openapi.seoul.go.kr:8088/${apiKey}/json/culturalEventInfo/${start}/${end}/${category}`;
       const { data } = await axios.get(url);
 
@@ -16,18 +17,21 @@ export const useFestivalStore = create((set) => ({
 
       // 데이터 구조를 안전하게 확인하고 설정
       if (data && data.culturalEventInfo && data.culturalEventInfo.row) {
-        set({ festivalData: data.culturalEventInfo.row });
+        const fetchedData = data.culturalEventInfo.row;
+        set({
+          festivalData: fetchedData,
+          filteredEvents: fetchedData, // 초기에 모든 이벤트를 filteredEvents에도 설정
+        });
       } else {
         console.error("예상치 못한 API 응답 구조:", data);
-        set({ festivalData: [] }); // 빈 배열로 설정
+        set({ festivalData: [], filteredEvents: [] }); // 빈 배열로 설정
       }
     } catch (error) {
       console.error("축제 데이터를 가져오는 중 오류 발생:", error);
-      set({ festivalData: [] }); // 오류 처리
+      set({ festivalData: [], filteredEvents: [] }); // 오류 처리
     } finally {
       set({ isLoading: false });
     }
   },
   setFilteredEvents: (events) => set({ filteredEvents: events }), // 필터링된 이벤트 설정
-
 }));
