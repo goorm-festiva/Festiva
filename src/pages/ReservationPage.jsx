ea6e2cdbc6456f248242c69ec0c28f6980118093
yungan9 <geyun105@naver.com>
2024-11-27 18:59:16 +0900

    design: 입력폼 디자인 추가

--- src/pages/ReservationPage.jsx
+++ src/pages/ReservationPage.jsx
@@ -13,26 +13,72 @@ export default function ReservationPage() {
   return (
     <>
       <Nav />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>예약자 성명을 입력해주세요.</label>
-          <input {...register("name")} />
-        </div>
-        <div>
-          <label>전화번호를 입력해주세요.</label>
-          <input {...register("phone")} />
-        </div>
-
-        <div>
-          <label>이메일을 입력해주세요.</label>
-          <input type="email" {...register("email")} />
-        </div>
-        <div>
-          <label>인원 수를 입력해주세요.</label>
-          <input {...register("email")} />
-        </div>
-        <button>submit</button>
-      </form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormContainer>
+          <Label>예약자 성명을 입력해주세요.</Label>
+          <Input placeholder="festiva@email.com" {...register("name")} />
+        </FormContainer>
+        <FormContainer>
+          <Label>전화번호를 입력해주세요.</Label>
+          <Input placeholder="010-0000-0000" {...register("phone")} />
+        </FormContainer>
+        <FormContainer>
+          <Label>이메일을 입력해주세요.</Label>
+          <Input
+            type="email"
+            placeholder="festiva@email.com"
+            {...register("email")}
+          />
+        </FormContainer>
+        <FormContainer>
+          <Label>인원 수를 입력해주세요.</Label>
+          <Input {...register("cnt")} />
+        </FormContainer>
+        <Btn>예약하기</Btn>
+      </Form>
     </>
   );
 }
+
+const Form = styled.div`
+  margin-left: 4%;
+`;
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  font-size: 16px;
+  font-weight: 400;
+  margin-left: 32px;
+`;
+
+const Input = styled.input`
+  height: 52px;
+  width: 40%;
+  margin-left: 40px;
+  margin-top: 8px;
+  margin-bottom: 40px;
+  padding-left: 8px;
+  font-size: 14px;
+  outline: none;
+  border-radius: 12px;
+  border: 1px solid lightgray;
+  &:focus {
+    border: 1px solid #141415;
+  }
+`;
+
+const Btn = styled.button`
+  margin-top: 28px;
+  margin-left: 40px;
+  width: 40%;
+  height: 52px;
+  border-radius: 15px;
+  font-size: 14px;
+  font-weight: 500;
+  color: #ffffff;
+  background-color: #000000;
+`;
