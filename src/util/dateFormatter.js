1d91ad86482873209bce806c8b419f2d39d3d822
jyooni <berry3891@naver.com>
2024-11-29 13:54:02 +0900

    refactor: dateFormatter 모듈화

---
+++ src/util/dateFormatter.js
@@ -0,0 +1,8 @@
+//날짜 포맷 변경
+const dateFormatter = (DATE) => {
+  if (!DATE) return "Invalid Date"; // DATE가 없을 경우 처리
+  const newDate = DATE.replaceAll("-", ".").replaceAll("~", " - ");
+  return newDate;
+};
+
+export default dateFormatter;
