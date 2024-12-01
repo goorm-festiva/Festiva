c8dc6e0c5f6c516cab12713f4032c9fecabb5da9
yungan9 <geyun105@naver.com>
2024-12-01 15:03:56 +0900

    feat: 날짜 선택 캘린더 추가

--- src/pages/ReservationPage.jsx
+++ src/pages/ReservationPage.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
+import { Calendar } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 import Nav from "../components/Nav";
+import { useState } from "react";
 
 export default function ReservationPage() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    console.log("Selected Date:", date);
+  };
 
   const onSubmit = (data) => {
     console.log(data);
@@ -17,44 +28,54 @@ export default function ReservationPage() {
   return (
     <>
       <Nav />
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <FormContainer>
-          <Label>예약자 성명을 입력해주세요.</Label>
-          <Input placeholder="festiva@email.com" {...register("name")} />
-        </FormContainer>
-        <FormContainer>
-          <Label>전화번호를 입력해주세요.</Label>
-          <Input placeholder="010-0000-0000" {...register("phone")} />
-        </FormContainer>
-        <FormContainer>
-          <Label>이메일을 입력해주세요.</Label>
-          <Input
-            type="email"
-            placeholder="festiva@email.com"
-            {...register("email")}
-          />
-        </FormContainer>
-        <FormContainer>
-          <Label>인원 수를 입력해주세요.</Label>
-          <Select {...register("cnt", { required: true })}>
-            <option value="">인원 수 선택</option>
-            {Array.from({ length: 10 }, (_, i) => (
-              <option key={i + 1} value={i + 1}>
-                {i + 1}명
-              </option>
-            ))}
-          </Select>
-          {errors.cnt && <ErrorText>인원 수를 선택해주세요.</ErrorText>}
-        </FormContainer>
-        <Btn>예약하기</Btn>
-      </Form>
+      <RsvContainer>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <FormContainer>
+            <Label>예약자 성명을 입력해주세요.</Label>
+            <Input placeholder="festiva@email.com" {...register("name")} />
+          </FormContainer>
+          <FormContainer>
+            <Label>전화번호를 입력해주세요.</Label>
+            <Input placeholder="010-0000-0000" {...register("phone")} />
+          </FormContainer>
+          <FormContainer>
+            <Label>이메일을 입력해주세요.</Label>
+            <Input
+              type="email"
+              placeholder="festiva@email.com"
+              {...register("email")}
+            />
+          </FormContainer>
+          <FormContainer>
+            <Label>인원 수를 입력해주세요.</Label>
+            <Select {...register("cnt", { required: true })}>
+              <option value="">인원 수 선택</option>
+              {Array.from({ length: 10 }, (_, i) => (
+                <option key={i + 1} value={i + 1}>
+                  {i + 1}명
+                </option>
+              ))}
+            </Select>
+            {errors.cnt && <ErrorText>인원 수를 선택해주세요.</ErrorText>}
+          </FormContainer>
+          <Btn>예약하기</Btn>
+        </Form>
+        <CalendarWrapper>
+          <Calendar date={selectedDate} onChange={handleDateChange} />
+        </CalendarWrapper>
+      </RsvContainer>
     </>
   );
 }
 
+const RsvContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 12%;
+`;
+
 const Form = styled.div`
   margin-left: 4%;
-  margin-top: 12%;
   width: 60%;
 `;
 
@@ -111,6 +132,8 @@ const ErrorText = styled.span`
   margin-left: 40px;
   margin-top: -30px;
 `;
+
+const CalendarWrapper = styled.div``;
 
 const Btn = styled.button`
   margin-top: 28px;
